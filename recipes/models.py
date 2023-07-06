1a2d7aab8b145d8fa6ead89dca1a96044e5fbd06
Brad Dunlap <braddunlapdpt@gmail.com>
2023-07-06 13:57:14 -0400

    Initial commit

---
+++ recipes/models.py
@@ -0,0 +1,12 @@
+from django.db import models
+
+class Recipe(models.Model):
+    title = models.CharField(max_length=255)
+    ingredients = models.TextField()
+    instructions = models.TextField()
+    details = models.TextField(blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
